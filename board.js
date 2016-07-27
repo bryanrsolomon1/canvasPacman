9cc2ebf9e38e88adeadf727d662c34424efc7e86
bryanrsolomon1 <bryanrsolomon1@gmail.com>
2016-07-27 15:21:53 -0500

    Initializing version control

---
+++ board.js
@@ -0,0 +1,111 @@
+/**
+ * Created by bryansolomon on 7/26/16.
+ * 
+ * I break the board out on its own so that I do not repaint its pieces each iteration. Saves major CPU
+ */
+
+/* global on the whole window because game.js needs access to this */
+var boardPieces = [];
+
+/**
+ * Having a lot of trouble with drawing the board to be dynamic based on the user's window. Floating point is making alley
+ * ways non-accessible and strange behavior results.
+ *
+ * Might need to hardcode everything in a little 500 x 500 the way the demo does
+ *
+ * @param width
+ * @param height
+ * @returns {number}
+ */
+var renderBoard = function (width, height) {
+
+    var board = $("#board").get(0);
+    var boardContext = board.getContext("2d");
+
+    board.width = width;
+    board.height = height;
+
+    var wall = 5;
+
+    var alley = (board.width - 2 * wall) / 26;
+
+    var pacmanRadius = alley / 2.25;
+
+    /* top */
+    boardPieces.push(new BoardPiece(0, 0, board.width, wall));
+    /* chiclet hanging down off top in the middle */
+    boardPieces.push(new BoardPiece(wall + 12 * alley, wall, 2 * alley, 4 * alley));
+
+    /* first row (4 squares across the top) */
+    boardPieces.push(new BoardPiece(wall + alley, wall + alley, 4 * alley, 3 * alley));
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + alley, 5 * alley, 3 * alley));
+    boardPieces.push(new BoardPiece(wall + 15 * alley, wall + alley, 5 * alley, 3 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + alley, 4 * alley, 3 * alley));
+
+    /* next set of shapes starting at (wall + 5 * alley) from the top */
+    /* had to do 5.1 because somewhere I'm missing something and pacman won't fit if just plain 5 */
+    /* possibly just floating point crap? */
+    boardPieces.push(new BoardPiece(wall + alley, wall + 5.1 * alley, 4 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 5.1 * alley, 2 * alley, 8 * alley));
+    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 5.1 * alley, 8 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 18 * alley, wall + 5.1 * alley, 2 * alley, 8 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 5.1 * alley, 4 * alley, 2 * alley));
+
+    /* bottom */
+    boardPieces.push(new BoardPiece(0, board.height - wall, board.width, wall));
+
+    /* left */
+    var sideWall = board.height - 2 * wall;
+
+    // top left
+    boardPieces.push(new BoardPiece(0, wall, wall, sideWall / 3));
+
+    //TODO: double check this middle logic is correct after changing wall logic
+    // middle left
+    // boardPieces.push(new BoardPiece(0, wall + sideWall / 3, board.width / 4, (sideWall / 3 - alley) / 2));
+    // boardPieces.push(new BoardPiece(0, wall + sideWall / 3 + (sideWall / 3 - alley) / 2 + alley, board.width / 4, (sideWall / 3 - alley) / 2));
+
+    // bottom left
+    // boardPieces.push(new BoardPiece(0, wall + 2 / 3 * sideWall, wall, sideWall / 3));
+    // boardPieces.push(new BoardPiece(wall, wall + 5 / 6 * sideWall, board.width / 10, wall));
+
+    /* right  */
+
+    // top right
+    boardPieces.push(new BoardPiece(board.width - wall, wall, wall, sideWall / 3));
+
+    // middle right
+    // boardPieces.push(new BoardPiece(board.width - board.width / 4, wall + sideWall / 3, board.width / 4, (sideWall / 3 - alley) / 2));
+    // boardPieces.push(new BoardPiece(board.width - board.width / 4, wall + sideWall / 3 + (sideWall / 3 - alley) / 2 + alley, board.width / 4, (sideWall / 3 - alley) / 2));
+
+    // bottom right
+    // boardPieces.push(new BoardPiece(board.width - wall, wall + 2 / 3 * sideWall, wall, sideWall / 3));
+    // boardPieces.push(new BoardPiece(board.width - wall - board.width / 10, wall + 5 / 6 * sideWall, board.width / 10, wall));
+
+    (function () {
+        boardContext.strokeStyle = "Black";
+
+        boardContext.fillRect(0, 0, board.width, board.height);
+
+        boardContext.save();
+
+        boardContext.strokeStyle = "Blue";
+        boardContext.lineWidth = 5;
+
+        boardPieces.forEach(function (piece) {
+            boardContext.strokeRect(piece.x, piece.y, piece.width, piece.height);
+        });
+
+        boardContext.restore();
+
+    })();
+
+    return pacmanRadius;
+
+    function BoardPiece(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+};
---
+++ game.js
@@ -0,0 +1,195 @@
+/**
+ * Created by bryansolomon on 7/26/16.
+ */
+$(document).ready(function () {
+
+    var DIRECTIONS = {
+        LEFT: {value: 1},
+        RIGHT: {value: 0},
+        DOWN: {value: 1.5},
+        UP: {value: .5}
+    };
+    DIRECTIONS.LEFT.opposite = DIRECTIONS.RIGHT.value;
+    DIRECTIONS.RIGHT.opposite = DIRECTIONS.LEFT.value;
+    DIRECTIONS.UP.opposite = DIRECTIONS.DOWN.value;
+    DIRECTIONS.DOWN.opposite = DIRECTIONS.UP.value;
+
+    var container = $("#body-wrapper");
+
+    var game = $("#game").get(0);
+    var gameContext = game.getContext("2d");
+
+    game.width = container.width();
+    game.height = container.height();
+    
+    var radius = renderBoard(game.width, game.height);
+
+    var pacman = new Pacman(radius, "Yellow");
+    var elements = [pacman];
+
+    function renderContent() {
+        /* TODO: add the ghosts to Elements array */
+        elements.forEach(function (element) {
+            gameContext.beginPath();
+
+            gameContext.fillStyle = element.fillStyle;
+
+            gameContext.translate(element.x, element.y);
+            gameContext.rotate(-element.direction.value * Math.PI);
+            gameContext.translate(-element.x, -element.y);
+
+            gameContext.arc(element.x, element.y, element.radius, element.startAngle * Math.PI, element.stopAngle * Math.PI, false);
+            gameContext.lineTo(element.x, element.y);
+
+            gameContext.fill();
+
+            gameContext.closePath();
+        })
+    }
+
+    function animationLoop() {
+        /* reset the canvas */
+        game.width = game.width;
+        /* paint the canvas */
+        renderContent();
+
+        /* update pacman for next iteration */
+
+        var updated = false;
+
+        /* is the user trying to turn? */
+        if (pacman.proposedDirection.value !== pacman.direction.value) {
+            updateMotion(pacman.proposedDirection.value);
+            if (isCollision()) {
+                /* can't turn that way, undo the motion */
+                updateMotion(pacman.proposedDirection.opposite);
+            } else {
+                pacman.direction = pacman.proposedDirection;
+                updated = true;
+                pacman.isMoving = true;
+            }
+        }
+
+        /* did the user turn? */
+        if (!updated) {
+            updateMotion(pacman.direction.value);
+            if (isCollision()) {
+                /* can't proceed on in current direction, undo the motion */
+                updateMotion(pacman.direction.opposite);
+                pacman.isMoving = false;
+            } else {
+                pacman.isMoving = true;
+            }
+        }
+
+        /* check wrap around */
+        if (pacman.x - pacman.radius > game.width) {
+            pacman.x = pacman.radius;
+            pacman.direction = DIRECTIONS.RIGHT;
+            pacman.proposedDirection = DIRECTIONS.RIGHT;
+        } else if (pacman.x + pacman.radius < 0) {
+            pacman.x = game.width - pacman.radius;
+            pacman.direction = DIRECTIONS.LEFT;
+            pacman.proposedDirection = DIRECTIONS.LEFT;
+        }
+
+        /* update mouth */
+        if (pacman.isMoving) {
+            if (pacman.mouthClosing) {
+                pacman.startAngle -= .05;
+                pacman.stopAngle += .05;
+                pacman.mouthClosing = pacman.startAngle > 0.05;
+            } else {
+                pacman.startAngle += .05;
+                pacman.stopAngle -= .05;
+                pacman.mouthClosing = pacman.startAngle > .20;
+            }
+        }
+
+        /* end update pacman */
+    }
+
+    function updateMotion(direction) {
+        switch (direction) {
+            case DIRECTIONS.RIGHT.value:
+                pacman.x += 5;
+                break;
+            case DIRECTIONS.LEFT.value:
+                pacman.x -= 5;
+                break;
+            case DIRECTIONS.DOWN.value:
+                pacman.y += 5;
+                break;
+            case DIRECTIONS.UP.value:
+                pacman.y -= 5;
+                break;
+        }
+    }
+
+    /* check collisions */
+    function isCollision() {
+        /* using a try-catch because there is no break statement in a JS for-each :( */
+        try {
+            boardPieces.forEach(function (piece) {
+                if (!(pacman.x + pacman.radius < piece.x) && // pacman too far right?
+                    !(piece.x + piece.width < pacman.x - pacman.radius) && // pacman too far left?
+                    !(pacman.y + pacman.radius < piece.y) && // pacman too low?
+                    !(piece.y + piece.height < pacman.y - pacman.radius)) { // pacman too high?
+                    console.log("Collision!");
+                    throw new Error(); // collision
+                }
+            });
+            return false;
+        } catch (e) {
+            return true;
+        }
+    }
+
+    window.addEventListener('keydown', doKeyDown, true);
+
+    function doKeyDown(evt) {
+        /* want to allow arrow keys or a-s-d-f controls */
+        switch (evt.keyCode) {
+            case 38:	// UP Arrow Key pressed
+                evt.preventDefault();
+            case 87:	// W pressed
+                pacman.proposedDirection = DIRECTIONS.UP;
+                break;
+            case 40:	// DOWN Arrow Key pressed
+                evt.preventDefault();
+            case 83:	// S pressed
+                pacman.proposedDirection = DIRECTIONS.DOWN;
+                break;
+            case 37:	// LEFT Arrow Key pressed
+                evt.preventDefault();
+            case 65:	// A pressed
+                pacman.proposedDirection = DIRECTIONS.LEFT;
+                break;
+            case 39:	// RIGHT Arrow Key pressed
+                evt.preventDefault();
+            case 68:	// D pressed
+                pacman.proposedDirection = DIRECTIONS.RIGHT;
+                break;
+            case 32:	// SPACE pressed -> pause
+                evt.preventDefault();
+                // TODO: pause the game
+                break;
+        }
+    }
+
+    setInterval(animationLoop, 30);
+
+    function Pacman(radius, fillStyle) {
+        this.startAngle = .25;
+        this.stopAngle = 1.75;
+        this.mouthClosing = true;
+        this.x = 6 + radius;
+        this.y = 6 + radius;
+        this.radius = radius;
+        this.direction = DIRECTIONS.RIGHT;
+        this.proposedDirection = null;
+        this.isMoving = false;
+        this.fillStyle = fillStyle;
+    }
+
+});
