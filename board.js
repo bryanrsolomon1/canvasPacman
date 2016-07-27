b088b539ed1fcaa675236c28e08cabdf6250ed7f
bryanrsolomon1 <bryanrsolomon1@gmail.com>
2016-07-27 17:42:26 -0500

    Finished board for now. Still need to work out bug related to movement not allowing pacman to enter alleys

--- board.js
+++ board.js
@@ -37,8 +37,6 @@ var renderBoard = function () {
 
     /* Board built from top down read from left to right */
 
-    var sideWall = board.height - 2 * wall; // account for top and bottom border
-
 
     /* top */
     boardPieces.push(new BoardPiece(0, 0, board.width, wall));
@@ -58,30 +56,87 @@ var renderBoard = function () {
     /* next set of shapes starting at (wall + 5 * alley) from the top */
     /* had to do 5.1 because somewhere I'm missing something and pacman won't fit if just plain 5 */
     /* possibly just floating point crap? */
-    boardPieces.push(new BoardPiece(wall + alley, wall + 5.1 * alley, 4 * alley, 2 * alley));
-    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 5.1 * alley, 2 * alley, 8 * alley));
-    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 5.1 * alley, 8 * alley, 2 * alley));
-    boardPieces.push(new BoardPiece(wall + 18 * alley, wall + 5.1 * alley, 2 * alley, 8 * alley));
-    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 5.1 * alley, 4 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + alley, wall + 5 * alley, 4 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 5 * alley, 2 * alley, 8 * alley));
+    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 5 * alley, 8 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 18 * alley, wall + 5 * alley, 2 * alley, 8 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 5 * alley, 4 * alley, 2 * alley));
 
     /* the bottom of the 'T' in the 'T' just above the ghost cage */
-    boardPieces.push(new BoardPiece(wall + 12 * alley, wall + 5.1 * alley + 2 * alley, 2 * alley, 3 * alley));
+    boardPieces.push(new BoardPiece(wall + 12 * alley, wall + 7 * alley, 2 * alley, 3 * alley));
 
-    /* the next row which includes:
-     * (1) the width-wise chunks of land that jet out from the sides
+    /* the next row includes:
+     * (1) the top width-wise chunks of land that jet out from the sides
      * (2) and the bottom of the 'T' in the sideways 'T's on the tall 8 alley bricks that stare at each other
-
-     /* (1) */
-    boardPieces.push(new BoardPiece(wall, wall + 8.1 * alley, 5 * alley, 5 * alley));
+     *
+     * (1) */
+    boardPieces.push(new BoardPiece(wall, wall + 8 * alley, 5 * alley, 5 * alley));
     /* (2) */
-    boardPieces.push(new BoardPiece(wall + 8 * alley, wall + 8.1 * alley, 3 * alley, 2 * alley));
-    boardPieces.push(new BoardPiece(wall + 15 * alley, wall + 8.1 * alley, 3 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 8 * alley, wall + 8 * alley, 3 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 15 * alley, wall + 8 * alley, 3 * alley, 2 * alley));
     /* (1) */
-    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 8.1 * alley, 5 * alley, 5 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 8 * alley, 5 * alley, 5 * alley));
 
-    /* bottom */
+    /* ghost box */
+    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 11 * alley, 8 * alley, 5 * alley));
+
+    /* bottom 1/2 left and right walls */
+    boardPieces.push(new BoardPiece(0, wall + 14 * alley, wall, 15 * alley));
+    boardPieces.push(new BoardPiece(board.width - wall, wall + 14 * alley, wall, 15 * alley));
+
+    /* the next row includes:
+     * (1) the bottom width-wise chunks of land that jet out from the sides
+     * (2) and the matching floating tall bricks between them
+     *
+     * (1) */
+    boardPieces.push(new BoardPiece(wall, wall + 14 * alley, 5 * alley, 5 * alley));
+    /* (2) */
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 14 * alley, 2 * alley, 5 * alley));
+    boardPieces.push(new BoardPiece(wall + 18 * alley, wall + 14 * alley, 2 * alley, 5 * alley));
+    /* (1) */
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 14 * alley, 5 * alley, 5 * alley));
+
+    /* the top of the 'T' underneath the ghost box */
+    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 17 * alley, 8 * alley, 2 * alley));
+
+    /* the bottom of the 'T' in the 'T' underneath the ghost cage */
+    boardPieces.push(new BoardPiece(wall + 12 * alley, wall + 19 * alley, 2 * alley, 3 * alley));
+
+    /* row full of straight flat lines that either float alone or are apart of the upside-down 'L's */
+    boardPieces.push(new BoardPiece(wall + alley, wall + 20 * alley, 4 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 20 * alley, 5 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 15 * alley, wall + 20 * alley, 5 * alley, 2 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 20 * alley, 4 * alley, 2 * alley));
+
+    /* the row that houses the dangling segments in the two upside-down 'L's */
+    boardPieces.push(new BoardPiece(wall + 3 * alley, wall + 22 * alley, 2 * alley, 3 * alley));
+    boardPieces.push(new BoardPiece(wall + 21 * alley, wall + 22 * alley, 2 * alley, 3 * alley));
+
+    /* (1) chiclets sprouting from the side of the walls
+     * (2) the bottoms of the upside-down mushrooms (shape equal to --> ____|_ & _|_____
+     * (3) top of the right side up 'T'
+     *
+     * (1) */
+    boardPieces.push(new BoardPiece(wall, wall + 23 * alley, 2 * alley, 2 * alley));
+    /* (2) */
+    boardPieces.push(new BoardPiece(wall + 6 * alley, wall + 23 * alley, 2 * alley, 3 * alley));
+    /* (3) */
+    boardPieces.push(new BoardPiece(wall + 9 * alley, wall + 23 * alley, 8 * alley, 2 * alley));
+    /* (2) */
+    boardPieces.push(new BoardPiece(wall + 18 * alley, wall + 23 * alley, 2 * alley, 3 * alley));
+    /* (1) */
+    boardPieces.push(new BoardPiece(board.width - wall - 2 * alley, wall + 23 * alley, 2 * alley, 2 * alley));
+
+    /* bottom of the 'T' */
+    boardPieces.push(new BoardPiece(wall + 12 * alley, wall + 25 * alley, 2 * alley, 3 * alley));
+
+    /* the bottom of the upside-down mushrooms (shape equal to --> ____|_ & _|_____    */
+     boardPieces.push(new BoardPiece(wall + alley, wall + 26 * alley, 10 * alley, 2 * alley));
+     boardPieces.push(new BoardPiece(wall + 15 * alley, wall + 26 * alley, 10 * alley, 2 * alley));
+
+     /* bottom */
     boardPieces.push(new BoardPiece(0, board.height - wall, board.width, wall));
-    
+
     (function () {
         boardContext.strokeStyle = "Black";
 
